a893064c4de8da72e38584b54f23488d06a4c441
admin-mahboob <mahboobk522@gmail.com>
2026-05-12 17:59:48 +0500

    Fix JitPack build environment: removed local JDK path and added jitpack.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.mahboobkhan"
             artifactId = "eline_monotization_sdk"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
